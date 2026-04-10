7ae9e7d555e76774dadc7e0bb25d80ea6ba69666
rafiprogrammer <rafi70226@gmail.com>
2026-04-10 16:26:01 +0600

    Project Section  Added

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-const lightMode=()=>{
-    document.body.style.background="white"
-    document.body.style.color="black"
-    document.body.style.transition="2s"
+const about = document.getElementById("about")
+const skill = document.getElementById("skill")
+const footer = document.getElementById("footer")
+const main = document.getElementById("light");
+// const description = document.getElementById("homepageDescription");
 
-}
+// const lightMode = () => {
+//     const currentBg = window.getComputedStyle(main).backgroundColor;
+
+//     if (currentBg === "rgb(11, 11, 13)") {
+//         main.style.background = "white";
+//         main.style.color = "#0B0B0D";
+//         main.style.transition = "1s";
+//         description.style.transition = "1s";
+//         description.style.color = "black"
+//         about.style.color = "white";
+//         skill.style.color = "white";
+//         footer.style.color = "white";
+//     }
+//     else{
+//         main.style.background = "#0B0B0D";
+//         main.style.color = "white";
+//         description.style.color = "white"
+//     }
+// }
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  const website = document.getElementById("website");
+
+  const minTime = 100; // minimum preloader time
+
+  setTimeout(function () {
+    preloader.style.opacity = "0"; // fade out
+    setTimeout(function() {
+      preloader.style.display = "none"; // remove preloader
+      website.style.display = "block";  // show website
+    }, 800);
+  }, minTime);
+});
